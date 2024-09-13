8e2771be17add41a1a610abc451f28e993e53acb
113265614xsp <0000113265614xsp@al.educacao.sp.gov.br>
2024-09-12 23:17:05 -0300

    adicionando arquivos js e css

---
+++ skecth.js
@@ -0,0 +1,13 @@
+function setup() {
+   createCanvas (600, 600);
+   background（"black");
+   }
+   function draw() {
+   stroke "blue");
+   fill ("red");
+   if(mouseIsPressed) (
+   rect(mouseX, mouseY, 20, 35);
+       }
+   }
+      
+            
